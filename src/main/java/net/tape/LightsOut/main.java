af8b6819a1149014f9b81f85aa16e399d47cf8e4
Tape Dispenser <gramiakshaye@gmail.com>
2024-07-29 12:01:51 -0600

    mod now works for versions 1.17+

--- src/main/java/net/tape/LightsOut/main.java
+++ src/main/java/net/tape/LightsOut/main.java
@@ -3,25 +3,7 @@ package net.tape.LightsOut;
 
 import net.fabricmc.api.ModInitializer;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class main implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("LightsOut");
-
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-
-
-		LOGGER.info("LightsOut successfully initialized.");
-
-	}
+	public void onInitialize() {}
 }
